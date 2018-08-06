da49b1c594648a633cdc62e4954eac3c72320cf0
ryo.ogi.0218 <ryo.ogi.0218@gmail.com>
2018-08-06 03:00:16 -0400

    message

--- image/ex6-5.js
+++ image/ex6-5.js
@@ -1,3 +1,4 @@
-function sayhello100(){var who='world';alert('Hello,'+who+'!');}
+var who='world';
+function sayhello(){alert('Hello,'+who+'!');}
 function taro(){who='太郎';}
 function hanako(){who='花子';}
